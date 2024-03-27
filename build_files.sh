deaab06ea1caddd9c49883546bc0b7ae2d724c85
ChengGong0602 <rjs0602@163.com>
2024-03-28 07:35:48 +0800

    first commit

---
+++ build_files.sh
@@ -0,0 +1,6 @@
+# build_files.sh
+pip install --upgrade pip --no-input
+pip install -r requirements.txt
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+python3.9 manage.py collectstatic --noinput
